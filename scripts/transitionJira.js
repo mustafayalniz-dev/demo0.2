157028d6df8199ca07518dde1a3995f94b1030d5
myalniz <myyalniz@gmail.com>
2020-12-09 17:53:21 +0300

    nn

--- scripts/transitionJira.js
+++ scripts/transitionJira.js
@@ -33,9 +33,11 @@ async function transitionIssues( issueKeys ) {
 		var isInReview = false
 		try {
     			isInReview = await jiraUtils.isInReview(issue_id)
+			console.log("IsInREview " + isInReview)
 		} catch(err) {
 			console.log(err)
 		}
+		console.log("ISINREVIEW " isInReview)
     		if (isInReview) {
       			console.log("Transitioning " + index + "th ticket " + issue_id + " to BuildReady")
       			await jiraUtils.transitionRequest(issue_id, jiraUtils.jiraTransitionIdBuildReady)
